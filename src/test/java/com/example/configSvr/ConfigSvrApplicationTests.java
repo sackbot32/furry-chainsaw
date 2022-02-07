7068b2f6323acfcc32541f5bca7f6d11dc081a96
Pablo Soler <sackbot32@gmail.com>
2022-02-07 14:43:11 +0100

    first commit

---
+++ src/test/java/com/example/configSvr/ConfigSvrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configSvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigSvrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
